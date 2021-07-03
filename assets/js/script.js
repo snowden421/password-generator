74b778d049fa9d07f7cb752062ea621a0a4b0425
Snow <84701509+snowden421@users.noreply.github.com>
2021-07-03 12:32:42 -0700

    defined generatePassword function, established prompt for password length, and confirm windows for character options.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
 // Assignment code here
 
+// define function generatePassword()
+function generatePassword() {
+  var passwordShown = "";
+  var passwordLength = 0;
+  var OptionLowerCase = false,
+  var OptionUpperCase = false,
+  var OptionNumeric = false,
+  var OptionSpecial = false;
+
+  // password length prompt
+  passwordLength = window.prompt("How long would you like your password? Please select a number between 8 and 128.");
+
+  // verify password length
+  if (passwordLength >= 8 && passwordLength <= 128) {
+
+    // if true, begin confirm prompts for lower case, upper case, numeric and special characters
+    OptionLowerCase = window.confirm("Would you like lower case letters?");
+    OptionUpperCase = window.confirm("Would you like upper case letters?");
+    OptionNumeric = window.confirm("Would you like numeric values?");
+    OptionSpecial = window.confirm("Would you like special characters?");
+
+    // verify confirmation of at least one character type
+    if (
+      OptionLowerCase === true ||
+      OptionUpperCase === true ||
+      OptionNumeric === true ||
+      OptionSpecial === true
+    ) {
+      return passwordShown = createPassword(passwordLength, [OptionLowerCase, OptionUpperCase, OptionNumeric, OptionSpecial]);
+    }
+    else {
+      window.alert("Please pick at least one character type.");
+    }
+  }
+  else {
+    window.alert("You have chosen an invalid password length. Please try again.");
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
